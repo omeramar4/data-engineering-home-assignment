a41bf894ad8fa6c71e07af0200caa4a32270cb89
oamar <oamar@yotpo.com>
2025-09-13 10:34:15 +0300

    add(job): stock stats Glue job

---
+++ assignment/objectives/stock_stats.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import posixpath
+from pyspark.sql.types import DateType
+
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import Window, functions as f, DataFrame as SparkDataFrame
+
+
+class BaseResultsHandler:
+
+    def handle(self, df: SparkDataFrame, name: str) -> None:
+        raise NotImplementedError('Subclasses must implement handle method')
+
+
+class PrintResultsHandler(BaseResultsHandler):
+
+    def handle(self, df: SparkDataFrame, name: str) -> None:
+        print(f'Results for {name}:')
+        df.show(truncate=False)
+
+
+class S3ParquetResultsHandler(BaseResultsHandler):
+
+    def __init__(self, output_base_path: str):
+        self.output_base_path = output_base_path
+
+    def handle(self, df: SparkDataFrame, name: str) -> None:
+        output_path = posixpath.join(self.output_base_path, name)
+        df.coalesce(1).write.mode('overwrite').parquet(output_path)
+        print(f'Results saved to {output_path}')
+
+
+class StockStats:
+
+    NUM_OF_TRADING_DAYS_IN_A_YEAR = 252
+
+    def __init__(self, data_path: str, results_handler: BaseResultsHandler = None):
+        self.data_path = data_path
+        self.results_handler = results_handler
+
+    @property
+    def spark(self) -> SparkSession:
+        return SparkSession.builder.getOrCreate()
+
+    def run(self):
+        df = self._load_data()
+        df_with_daily_return = self._get_daily_return(df)
+
+        # Objective 1: Average daily return
+        average_daily_return = self._get_average_daily_return(df_with_daily_return)
+        self.results_handler.handle(average_daily_return, "objective_1_average_daily_return")
+
+        # Objective 2: Highest average trade worth
+        highest_average_trade_worth = self._get_highest_average_trade_worth(df)
+        self.results_handler.handle(highest_average_trade_worth, "objective_2_highest_trade_worth")
+
+        # Objective 3: Most volatile stock
+        most_volatile_stock = self._get_most_volatile_stock(df_with_daily_return)
+        self.results_handler.handle(most_volatile_stock, "objective_3_most_volatile_stock")
+
+        # Objective 4: Top three 30-day returns
+        top_three_30d_returns = self._get_top_three_30d_returns(df)
+        self.results_handler.handle(top_three_30d_returns, "objective_4_top_30d_returns")
+
+    def _load_data(self):
+        return self.spark.read.csv(self.data_path, header=True, inferSchema=True)
+
+    @staticmethod
+    def _get_daily_return(df: SparkDataFrame) -> SparkDataFrame:
+        window_spec = Window.partitionBy('ticker').orderBy('Date')
+        return (
+            df
+            .withColumn('prev_close', f.lag('close').over(window_spec))
+            .withColumn('daily_return', (f.col('close') - f.col('prev_close')) / f.col('prev_close'))
+            .drop('prev_close')
+        )
+
+    @staticmethod
+    def _get_average_daily_return(df: SparkDataFrame) -> SparkDataFrame:
+        return (
+            df
+            .groupBy('Date')
+            .agg(f.avg('daily_return').alias('average_return'))
+            .withColumn('date', f.col('Date').cast(DateType()))
+            .select('date', 'average_return')
+        )
+
+    @staticmethod
+    def _get_highest_average_trade_worth(df: SparkDataFrame) -> SparkDataFrame:
+        return (
+            df
+            .withColumn('trade_worth', f.col('close') * f.col('volume'))
+            .groupBy('ticker')
+            .agg(f.avg('trade_worth').alias('value'))
+            .orderBy(f.desc('value'))
+            .limit(1)
+        )
+
+    def _get_most_volatile_stock(self, df: SparkDataFrame) -> SparkDataFrame:
+        return (
+            df
+            .groupBy('ticker')
+            .agg(
+                (
+                    f.stddev('daily_return') * f.sqrt(f.lit(self.NUM_OF_TRADING_DAYS_IN_A_YEAR))
+                ).alias('standard_deviation')
+            )
+            .orderBy(f.desc('standard_deviation'))
+            .limit(1)
+        )
+
+    @staticmethod
+    def _get_top_three_30d_returns(df: SparkDataFrame) -> SparkDataFrame:
+        window_spec = Window.partitionBy('ticker').orderBy('Date')
+
+        return (
+            df
+            .withColumn('close_30d_ago', f.lag('close', 30).over(window_spec))
+            .withColumn('return_30d', ((f.col('close') - f.col('close_30d_ago')) / f.col('close_30d_ago')))
+            .withColumn('date', f.col('Date').cast(DateType()))
+            .select('ticker', 'date', 'return_30d')
+            .orderBy(f.desc('return_30d'))
+            .drop('return_30d')
+            .limit(3)
+        )
+
+
+if __name__ == '__main__':
+    args = getResolvedOptions(sys.argv, ["JOB_NAME", "S3_BUCKET", "DATA_PATH"])
+    sc = SparkContext()
+    glueContext = GlueContext(sc)
+    spark = glueContext.spark_session
+    job = Job(glueContext)
+    job.init(args["JOB_NAME"], args)
+
+    stocks_stats = StockStats(
+        data_path=args["DATA_PATH"],
+        results_handler=S3ParquetResultsHandler(output_base_path=f"s3://{args['S3_BUCKET']}/results")
+    )
+    stocks_stats.run()
+    job.commit()
